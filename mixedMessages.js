f64f803925616c32bfb8ad49794b140c6c5a878d
Jason Chan <jchan321738@gmail.com>
2021-04-17 16:50:02 -0700

    add jokes

--- mixedMessages.js
+++ mixedMessages.js
@@ -1,4 +1,4 @@
-const jokesArray = [["Where do you find a cow with no legs?", "Right where you left it."], ["How does a squid go into battle?", "Well-armed"], ["What's the best thing about Switzerland?", "I don't know, but their flag is a huge plus"], ["Why aren't koalas actual bears?", "They don't meet the koalafications"]];
+const jokesArray = [["Where do you find a cow with no legs?", "Right where you left it."], ["How does a squid go into battle?", "Well-armed"], ["What's the best thing about Switzerland?", "I don't know, but their flag is a huge plus"], ["Why aren't koalas actual bears?", "They don't meet the koalafications"],["What do Alexander the Great and Winnie the Pooh have in common?", "Same middle name"], ["What is the opposite of a croissant?", "A happy uncle"], ["Which branch of the military accepts toddlers?", "The infantry"], ["What do the movies Titanic and The Sixth Sense have in common?", "Icy dead people"], ["What do a tick and the Eiffel Tower have in common?", "They're both Paris sites"]];
 let num = Math.floor(Math.random() * jokesArray.length)
 
 function chooseJoke() {
